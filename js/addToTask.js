e86add70957a522ed32402f7b5acbbe4118b7b34
Alexander Baraev <alexander.baraev95@googlemail.com>
2022-01-31 19:15:43 +0100

    addToTask.js added functions

---
+++ js/addToTask.js
@@ -0,0 +1,102 @@
+let users = [
+    {
+        'userName': 'Alex',
+        'userImage': '../img/logo.png'
+    },
+    {
+        'userName': 'Willi',
+        'userImage': '../img/AddTask.PNG'
+    },
+    {
+        'userName': 'Georg',
+        'userImage': '../img/Backlog.PNG'
+    }
+];
+
+let allTasks = [];
+
+let plusButton = '../img/icon plus.png'
+
+function loadUsers() {
+    let assignedUsers = document.getElementById('assigned-to-container');
+
+    for (let i = 0; i < users.length; i++) {
+        assignedUsers.innerHTML += `
+            <div class="user-profile">
+                <img id="user-id-${i}" src='${users[i]['userImage']}' onclick="assignedToUser('${users[i]['userName']}', ${i})">
+                <div>${users[i]['userName']}</div>
+            </div>
+        `;
+    }
+    assignedUsers.innerHTML += `
+        <img src='${plusButton}'>
+    `;
+}
+
+function assignedToUser(username, index) {
+    let selectedUser = document.getElementById('selected-user');
+    selectedUser.value = username;
+
+    highlightSelectedUser(index);
+}
+
+function highlightSelectedUser(index){
+    deleteAllHighlights();
+    document.getElementById('user-id-' + index).classList.add('highlight-selected-user');
+}
+
+function deleteAllHighlights() {
+    for(let i = 0; i < users.length; i++) {
+        document.getElementById('user-id-' + i).classList.remove('highlight-selected-user');
+    }
+}
+
+
+function cancelCommand() {
+    let popup = document.getElementById('clearPopup');
+
+    popup.style = 'display: flex';
+}
+
+function clearAll() {
+    document.getElementById('taskTitle').value = '';
+    document.getElementById('select-date').value = '';
+    document.getElementById('textarea').value = '';
+    document.getElementById('selected-user').value = '';
+    deleteAllHighlights();
+
+    document.getElementById('clearPopup').style = 'display: none;'
+
+}
+
+function noClearing() {
+    document.getElementById('clearPopup').style = 'display: none;'
+}
+
+function createTask() {
+    let title = document.getElementById('taskTitle').value;
+    let date = document.getElementById('select-date').value;
+    let category = document.getElementById('category');
+    let categorySelected = category.options[category.selectedIndex].text;
+    let urgency = document.getElementById('urgency');
+    let urgencySelected = urgency.options[urgency.selectedIndex].text;
+    let description = document.getElementById('textarea').value;
+    let selectedUser = document.getElementById('selected-user').value;
+
+    let task = {
+        'title': title,
+        'date': date,
+        'category': categorySelected,
+        'urgency': urgencySelected,
+        'description': description,
+        'assignedTo': selectedUser
+    };
+
+    allTasks.push(task);
+    clearAll();
+    deleteAllHighlights();
+
+}
+
+
+
